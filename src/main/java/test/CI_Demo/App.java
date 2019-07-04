1cbd46bb0f8306b56aec0fa747e609bcd8ef0ff0
Pesonen Jarkko <Jarkko.Pesonen@etteplan.com>
2019-07-04 16:28:37 +0300

    Adding demo project

---
+++ src/main/java/test/CI_Demo/App.java
@@ -0,0 +1,13 @@
+package test.CI_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n" );
+    }
+}
