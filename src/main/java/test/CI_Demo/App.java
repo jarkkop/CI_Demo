b596fceaa3cdf91e51405ba5ff0177737e5006b0
Pesonen Jarkko <Jarkko.Pesonen@etteplan.com>
2019-07-05 11:48:55 +0300

    2nd edition

--- src/main/java/test/CI_Demo/App.java
+++ src/main/java/test/CI_Demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n" );
+        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n 2nd edition\n" );
     }
 }
