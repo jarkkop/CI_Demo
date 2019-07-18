72dae8c5a1aaf66259696e7b62e6624992690669
Pesonen Jarkko <Jarkko.Pesonen@etteplan.com>
2019-07-18 10:12:59 +0300

    4th edition, Manifest files nd new pom with addDefaultImplementationEntries and  addDefaultSpecificationEntries

--- src/main/java/test/CI_Demo/App.java
+++ src/main/java/test/CI_Demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n 3rd edition\n" );
+        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n 4thd edition\n" );
     }
 }
