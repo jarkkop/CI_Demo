3db3392b82c5e551a9feae484e1ac93e4f411eb1
Pesonen Jarkko <Jarkko.Pesonen@etteplan.com>
2019-07-18 12:07:13 +0300

    5th edition, lowercase in names

--- src/main/java/test/CI_Demo/App.java
+++ src/main/java/test/CI_Demo/App.java
@@ -1,4 +1,4 @@
-package test.CI_Demo;
+package test.ci_demo;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n 4thd edition\n" );
+        System.out.println( "\n Hello World!\n This is a Demo project for CI pipe with Jenkins\n 5thd edition\n" );
     }
 }
--- src/test/java/test/CI_Demo/AppTest.java
+++ src/test/java/test/CI_Demo/AppTest.java
@@ -1,4 +1,4 @@
-package test.CI_Demo;
+package test.ci_demo;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
